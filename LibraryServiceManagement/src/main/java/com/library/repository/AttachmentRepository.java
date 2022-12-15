bd7dac18d4db9ebe3a05e6c01db9f184a17ff5f0
VHoangDotCom <viethoang2001gun@gmail.com>
2022-12-15 14:42:03 +0700

    #Task1.3 - Hoang

---
+++ LibraryServiceManagement/src/main/java/com/library/repository/AttachmentRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+import com.library.entity.attachment.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, String> {
+}
+
